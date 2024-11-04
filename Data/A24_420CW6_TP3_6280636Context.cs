1ab2a63c630d493bf5d45499a8c95227c2967055
victor <6280636@cegepmontpetit.ca>
2024-11-04 18:54:54 -0500

    FCT Creation de model et controller
    Creation de la clase Score avec son controlleur, add de nugets

---
+++ Data/A24_420CW6_TP3_6280636Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using A24_420CW6_TP3_6280636.Models;
+
+namespace A24_420CW6_TP3_6280636.Data
+{
+    public class A24_420CW6_TP3_6280636Context : DbContext
+    {
+        public A24_420CW6_TP3_6280636Context (DbContextOptions<A24_420CW6_TP3_6280636Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<A24_420CW6_TP3_6280636.Models.Score> Score { get; set; } = default!;
+    }
+}
