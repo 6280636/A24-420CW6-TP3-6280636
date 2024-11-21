25346da44a69a349db77467f6700a1f97fd33941
victor <6280636@cegepmontpetit.ca>
2024-11-21 11:17:35 -0500

    FCT Creation de UserController
    Creation de la methode loginDTO avec sa classe,
    installation de la package JwtBearer

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using A24_420CW6_TP3_6280636.Models;
+using A24_420CW6_TP3_6280636.Models.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -43,41 +44,42 @@ namespace A24_420CW6_TP3_6280636.Controllers
             return Ok(new { Message = "Inscription reussie!"});
         }
 
-        //[HttpPost]
-        //public async Task<ActionResult> Login(LoginDTO login)
-        //{
-        //    User user = await UserManager.FindByNameAsync(login.Username);
-        //    if (user != null && await UserManager.CheckPasswordAsync(user, login.Password))
-        //    {
-        //        IList<string> roles = await UserManager.GetRolesAsync(user);
-        //        List<Claim> authClaims = new List<Claim>();
-        //        foreach (string role in roles)
-        //        {
-        //            authClaims.Add(new Claim(ClaimTypes.Role, role));
-        //        }
-        //        authClaims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id));
-        //        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8
-        //            .GetBytes("LoooOOongue Phrase SiNon PaPaapa"));
-        //        JwtSecurityToken token = new JwtSecurityToken(
-        //            issuer: "https://localhost:7234",
-        //            audience: "http://localhost:4200",
-        //            claims: authClaims,
-        //            expires: DateTime.Now.AddMinutes(30),
-        //            signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
-        //            );
-        //        return Ok(new
-        //        {
-        //            token = new JwtSecurityTokenHandler().WriteToken(token),
-        //            validTo = token.ValidTo
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginDTO login)
+        {
+            User user = await UserManager.FindByNameAsync(login.Username);
+            if (user != null && await UserManager.CheckPasswordAsync(user, login.Password))
+            {
+                IList<string> roles = await UserManager.GetRolesAsync(user);
+                List<Claim> authClaims = new List<Claim>();
+                foreach (string role in roles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+                authClaims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8
+                    .GetBytes("LooOOongue Phrase SiNoN Ca ne Marchera PaPaapa"));
+                JwtSecurityToken token = new JwtSecurityToken(
+                    issuer: "https://localhost:7000",
+                    audience: "http://localhost:4200",
+                    claims: authClaims,
+                    expires: DateTime.Now.AddMinutes(300),
+                    signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+                    );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    validTo = token.ValidTo,
+                    id = user.Id
 
-        //        });
-        //    }
-        //    else
-        //    {
-        //        return StatusCode(StatusCodes.Status400BadRequest,
-        //            new { Message = "Le nom de utilisateur est invalide" });
-        //    }
+                });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status400BadRequest,
+                    new { Message = "Le nom de utilisateur ou le mot de passe est invalide" });
+            }
 
-        //}
+        }
     }
 }
---
+++ Models/DTOs/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace A24_420CW6_TP3_6280636.Models.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
